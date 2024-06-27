9004cf68c256b70f1a667219e78cee04e5d582d7
Luiggi <luiggiyantas@gmail.com>
2024-06-27 18:05:19 -0500

    add url netlify

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:4200"]
+    origin: ["http://localhost:4200", "https://el-criollo.netlify.app"]
 }))
 
 // usando las apis del archivo food.router.ts (enrutador de alimentos)
